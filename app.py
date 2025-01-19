36588e6ccd03002515d20eb6a8ea04988b9dc9ad
Paras Sahu <paras.sahu@tesco.com>
2025-01-19 21:48:28 +0530

    Completed the first phase of Search Tool by providing a context to LLM using Langchain

--- app.py
+++ app.py
@@ -9,8 +9,7 @@ from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
 
 from dotenv import load_dotenv
-load_dotenv()  # take environment variables from .env (especially openai api key)
-
+load_dotenv()
 # Custom CSS for the sidebar
 sidebar_css = """
 <style>
